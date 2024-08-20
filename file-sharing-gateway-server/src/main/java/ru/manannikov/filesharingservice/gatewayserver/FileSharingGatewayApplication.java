85137f8b65ae490e22c3314d1d4021b5970adc6b
stradiavanti <anton2mana2@gmail@gmail.com>
2024-08-20 23:20:43 +0300

    небольшие правки

--- file-sharing-gateway-server/src/main/java/ru/manannikov/filesharingservice/gatewayserver/FileSharingGatewayApplication.java
+++ file-sharing-gateway-server/src/main/java/ru/manannikov/filesharingservice/gatewayserver/FileSharingGatewayApplication.java
@@ -1,11 +1,8 @@
-package ru.manannikov.filesharingservice.gateway;
+package ru.manannikov.filesharingservice.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RefreshScope
