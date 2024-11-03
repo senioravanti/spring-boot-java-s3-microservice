506738ccc5201a0e2378e03fc44b333385d06ff1
Manannikov Anton <anton2manan2@gmail.com>
2024-11-03 12:28:03 +0300

    feat: setup keycloak -- oauth2 authorization server implementation.

---
+++ file-sharing-s3-service/src/main/java/ru/manannikov/filesharingservice/s3/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package ru.manannikov.filesharingservice.s3.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder
+//            .withJwkSetUri(jwkSetUri).build();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception
+    {
+        http.csrf(
+            AbstractHttpConfigurer::disable
+        );
+
+        http.sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.oauth2ResourceServer(
+            auth2ResourceServer -> auth2ResourceServer
+                .jwt(jwt -> jwt
+                    .jwkSetUri(jwkSetUri)
+                )
+        );
+
+        http.authorizeHttpRequests(
+            auth -> auth
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
